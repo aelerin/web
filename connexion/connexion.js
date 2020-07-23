0593b4d0c76a1a5ca23c8b4fb9100db7fb7deb7a
Aelerin <allegaert.kevinffbe@gmail.com>
2020-07-23 14:10:14 +0200

    "second commit"

--- connexion/connexion.js
+++ connexion/connexion.js
@@ -1,13 +1,22 @@
-let Utilisateur = {
-    
-}
+let Utilisateur = [
+    { pseudo: "Michel", password: "Banane" },
+    { pseudo: "Martine", password: "Jambon" },
+    { pseudo: "Mariette", password: "Orange" },
+    { pseudo: "Kiki", password: "crevette" },
+    { pseudo: "root", password: "root" }
+]
 
-const send = (event) => {
+const connexion = (event) => {
     event.preventDefault()
-    Utilisateur[event.target[0].name] = event.target[0].value
-    Utilisateur[event.target[1].name] = event.target[1].value
-    console.log(Utilisateur)
+    for (users of Utilisateur) {
+        console.log(users)
+        if (users.pseudo == event.target[0].value && users.password == event.target[1].value) {
+            if (confirm("Felicitations, vous êtes maintenant connecter")) {
+                window.location.href = "../accueil/accueil.html";
+            }
+
+        }
+    }
 }
 
-document.querySelector('#MonForm').addEventListener
-('submit', send)
+document.querySelector("#MonForm").addEventListener('submit', connexion)
--- recherche/recherche.js
+++ recherche/recherche.js
@@ -1,8 +1,8 @@
-let trouvaille = [ 
-    {name: "Michel",lastname: "Vedette",contact: "Michel@Vedette.fr"},
-    {name: "Martine",lastname: "Vedette",contact: "Michel@Vedette.fr"},
-    {name: "Mariette",lastname: "Vedette",contact: "Michel@Vedette.fr"},
-    {name: "Kiki",lastname: "Vedette",contact: "Michel@Vedette.fr"},
+let trouvaille = [
+    { name: "Michel", lastname: "Vedette", contact: "Michel@Vedette.fr" },
+    { name: "Martine", lastname: "Vedette", contact: "Michel@Vedette.fr" },
+    { name: "Mariette", lastname: "Vedette", contact: "Michel@Vedette.fr" },
+    { name: "Kiki", lastname: "Vedette", contact: "Michel@Vedette.fr" },
 ]
 
 
@@ -10,10 +10,10 @@ const generateTableHead = (table, data) => {
     let thead = table.createTHead();
     let row = thead.insertRow();
     for (let key of data) {
-      let th = document.createElement("th");
-      let text = document.createTextNode(key);
-      th.appendChild(text);
-      row.appendChild(th);
+        let th = document.createElement("th");
+        let text = document.createTextNode(key);
+        th.appendChild(text);
+        row.appendChild(th);
     }
 }
 
@@ -22,13 +22,13 @@ const generateTable = (table, data) => {
     for (let element of data) {
         let row = table.insertRow()
         for (key in element) {
-        let cell = row.insertCell()
-        let text = document.createTextNode(element[key])
-        cell.appendChild(text);
+            let cell = row.insertCell()
+            let text = document.createTextNode(element[key])
+            cell.appendChild(text);
         }
     }
 }
-  
+
 function myFunction() {
 
     var input, filter, table, tr, td, i, txtValue;
@@ -41,19 +41,20 @@ function myFunction() {
     for (i = 0; i < tr.length; i++) {
         td = tr[i].getElementsByTagName("td")[0];
         if (td) {
-        txtValue = td.textContent || td.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            tr[i].style.display = "";
-        } else {
-            tr[i].style.display = "none";
-        }
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
         }
     }
 }
 
 
-  let table = document.querySelector("table")
-  let tbody = document.querySelector("tbody")
-  let data = Object.keys(trouvaille[0]);
-  generateTableHead(table, data);
-  generateTable(tbody, trouvaille);
+let table = document.querySelector("table")
+let tbody = document.querySelector("tbody")
+let data = Object.keys(trouvaille[0]);
+generateTableHead(table, data);
+generateTable(tbody, trouvaille);
+document.querySelector("#myInput").addEventListener("keyup", myFunction);
